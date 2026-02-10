4113db25ab34b6922a27ddb52b48cdcf74477386
PGaillot <pr.gaillot@gmail.com>
2026-02-10 20:10:41 +0100

    feat(vault-lib): add new Angular library with initial setup and configuration
    - Updated package.json to include @angular/build and ng-packagr.
    - Modified tsconfig.json to add path mapping and project references.
    - Created README.md for vault-lib with build and testing instructions.
    - Added ng-package.json for library packaging configuration.
    - Introduced package.json for vault-lib with peer dependencies.
    - Implemented initial VaultLib component and its corresponding unit tests.
    - Created public API surface for vault-lib.
    - Configured TypeScript settings for library and production builds.
    - Set up TypeScript configuration for unit testing.

---
+++ projects/vault-lib/src/lib/vault-lib.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-vault-lib',
+  imports: [],
+  template: `
+    <p>
+      vault-lib works!
+    </p>
+  `,
+  styles: ``,
+})
+export class VaultLib {
+
+}
---
+++ projects/vault-lib/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of vault-lib
+ */
+
+export * from './lib/vault-lib';
