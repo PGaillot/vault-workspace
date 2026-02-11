702c7247b96cab1d436cc24f382d35034b5a9ad1
PGaillot <pr.gaillot@gmail.com>
2026-02-11 18:42:10 +0100

    refacto & hover Invert directive.

---
+++ projects/vault-lib/src/lib/directives/hover-invert.spec.ts
@@ -0,0 +1,8 @@
+import { HoverInvert } from './hover-invert';
+
+describe('HoverInvert', () => {
+  it('should create an instance', () => {
+    const directive = new HoverInvert();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/vault-lib/src/lib/directives/hover-invert.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[vaultHoverInvert]',
+})
+export class HoverInvert {
+  constructor() {}
+}
