af0286dee838f18be55b16e9bf92bd4509fddaf7
PGaillot <pr.gaillot@gmail.com>
2026-02-19 23:58:24 +0100

    swap spinner and progress-bar stories in documentation.json; update public API to include progress-bar

--- projects/vault-lib/src/public-api.ts
+++ projects/vault-lib/src/public-api.ts
@@ -7,6 +7,7 @@ export * from './lib/vault-lib';
 export * from './lib/components/button/button';
 export * from './lib/components/fieldset/fieldset';
 export * from './lib/components/spinner/spinner';
+export * from './lib/components/progress-bar/progress-bar';
 export * from './lib/components/taskbar/taskbar';
 export * from './lib/components/system-icon/system-icon';
 
