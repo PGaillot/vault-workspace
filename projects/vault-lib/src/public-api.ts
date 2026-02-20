512974a83309e7acfd98177782af34884e6d9b81
PGaillot <pr.gaillot@gmail.com>
2026-02-20 18:36:58 +0100

    fix: export system-window component in public API

--- projects/vault-lib/src/public-api.ts
+++ projects/vault-lib/src/public-api.ts
@@ -10,6 +10,7 @@ export * from './lib/components/spinner/spinner';
 export * from './lib/components/progress-bar/progress-bar';
 export * from './lib/components/taskbar/taskbar';
 export * from './lib/components/system-icon/system-icon';
+export * from './lib/components/system-window/system-window';
 
 export * from './lib/directives/directive.index';
 
