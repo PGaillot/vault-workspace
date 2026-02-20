d9082873cab58ef68f083a6f31635354a28eaef3
PGaillot <pr.gaillot@gmail.com>
2026-02-20 18:53:18 +0100

    refactor: update documentation and remove unused components from public API

--- projects/vault-lib/src/public-api.ts
+++ projects/vault-lib/src/public-api.ts
@@ -2,8 +2,6 @@
  * Public API of vault-lib
  */
 
-export * from './lib/vault-lib';
-
 export * from './lib/components/button/button';
 export * from './lib/components/fieldset/fieldset';
 export * from './lib/components/spinner/spinner';
